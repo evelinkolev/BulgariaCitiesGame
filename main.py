f8e85ca2bd67fa8d854dc1ab639d305d6a718165
Evelin Kolev <evelinkolev@vivaldi.net>
2024-04-14 11:28:36 +0200

    Set up screen

---
+++ main.py
@@ -0,0 +1,12 @@
+import turtle
+
+screen = turtle.Screen()
+screen.title("Bulgaria Cities Game")
+
+image = "bulgaria_blank_map.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+
+
+screen.exitonclick()
