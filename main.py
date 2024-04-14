a8016f0aa3c89eb0eeabf294e2e86cd33f5de184
Evelin Kolev <evelinkolev@vivaldi.net>
2024-04-14 13:12:15 +0200

    Saving data to .csv

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("Bulgaria Cities Game")
@@ -6,14 +7,52 @@ screen.title("Bulgaria Cities Game")
 image = "bulgaria_blank_map.gif"
 screen.addshape(image)
 turtle.shape(image)
+screen.setup(1.0, 1.0)
+
+data = pandas.read_csv("cities.csv")
+all_cities = data.city.to_list()
+#print(len(all_cities))
+guessed_cities = []
+
+while len(guessed_cities) < 27:
+    answer_city = screen.textinput(title=f"{len(guessed_cities)}/27 Cities Correct",
+                                   prompt="What's another city's name?").title()
+    #print(answer_city)
+    if answer_city == "Exit":
+        missing_cities = []
+        for city in all_cities:
+            if city not in guessed_cities:
+                missing_cities.append(city)
+        #print(missing_cities)
+        new_data = pandas.DataFrame(missing_cities)
+        new_data.to_csv("cities_to_learn.csv")
+        break
+
+    if answer_city in all_cities:
+        guessed_cities.append(answer_city)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        city_data = data[data.city == answer_city]
+        t.goto(int(city_data.x), int(city_data.y))
+        t.write(city_data.city.item())
 
 
-def get_mouse_click_coor(x, y):
-    print(x, y)
 
 
-turtle.onscreenclick(get_mouse_click_coor)
 
-turtle.mainloop()
 
-screen.exitonclick()
+
+
+
+
+
+#def get_mouse_click_coor(x, y):
+    #print(x, y)
+
+
+#turtle.onscreenclick(get_mouse_click_coor)
+
+#turtle.mainloop()
+
+#screen.exitonclick()
