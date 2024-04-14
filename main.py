15b1fcc4b1764f53b5e3848486842af56c35381f
Evelin Kolev <evelinkolev@vivaldi.net>
2024-04-14 11:42:28 +0200

    Get the coordinates of a mouse click in the turtle screen

--- main.py
+++ main.py
@@ -8,5 +8,12 @@ screen.addshape(image)
 turtle.shape(image)
 
 
+def get_mouse_click_coor(x, y):
+    print(x, y)
+
+
+turtle.onscreenclick(get_mouse_click_coor)
+
+turtle.mainloop()
 
 screen.exitonclick()
